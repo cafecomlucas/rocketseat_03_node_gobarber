2760452fc663c2559f13cc4e45e3ce197a996df1
Lucas Soares <cafecomlucas@gmail.com>
2019-08-20 11:55:40 -0300

    Estrutura do servidor | Define rota principal

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const routes = require("./routes");
 
 class App {
   constructor() {
     this.server = express();
+
+    this.routes();
+  }
+
+  routes() {
+    this.server.use(routes);
   }
 }
 
