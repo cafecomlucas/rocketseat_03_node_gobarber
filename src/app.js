6de4e93a4e962853cb8f2d8ed607aa6bd74a0aee
Lucas Soares <cafecomlucas@gmail.com>
2019-08-20 12:35:48 -0300

    Estrutura do servidor | Cria middleware para aceitar requisições com JSON

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const routes = require("./routes");
 
+/**
+ * Estrutura da aplicação
+ */
+
+// Faz sentido utilizarmos classes (principalmente no back-end)
 class App {
+  // Método chamado ao instanciar essa classe
   constructor() {
+    // cria uma instância do express
     this.server = express();
 
+    // configura os middleares e as rotas
+    this.middlewares();
     this.routes();
   }
 
+  // método com a configuração de todos os middlewares
+  middlewares() {
+    // middleware para receber requisições no formato JSON
+    this.server.use(express.json());
+  }
+
+  // método com a configuração de todas as rotas
   routes() {
     this.server.use(routes);
   }
 }
 
+// exporta a propriedade server da instância de App
 module.exports = new App().server;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const { Router } = require("express");
 
+/**
+ * Configuração do Roteamento que será utilizado pela aplicação
+ */
+
 const routes = new Router();
 
 routes.get("/", (req, res) => res.json({ message: "Hello world" }));
