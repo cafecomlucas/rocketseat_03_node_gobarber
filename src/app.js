92bcdc138e63f5772c6fde0db8fcc8388de2c936
Lucas Soares <cafecomlucas@gmail.com>
2019-08-20 11:47:01 -0300

    Configura estrutura do servidor | Separa 'app' do 'server'

--- src/app.js
+++ src/app.js
@@ -3,10 +3,7 @@ const express = require("express");
 class App {
   constructor() {
     this.server = express();
-    this.server.listen(3333, () =>
-      console.log("Server listening on port 3333...")
-    );
   }
 }
 
-module.exports = new App();
+module.exports = new App().server;
